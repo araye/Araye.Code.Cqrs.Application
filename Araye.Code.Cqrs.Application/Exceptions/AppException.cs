4e2ac773e15d6813f4a3be6d2e8de14abe67a01b
Hamed Saeedi <hameds@gmail.com>
2019-05-08 13:24:24 +0430

    add AppExecption to avoid returning 500 server errors

---
+++ Araye.Code.Cqrs.Application/Exceptions/AppException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Araye.Code.Cqrs.Application.Exceptions
+{
+    public class AppException : Exception
+    {
+        public AppException(string message)
+            : base($"An error occured: {message}")
+        {
+        }
+    }
+}
